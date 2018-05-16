6e6e4dd01dd55d2ddf6d6316e3f2baccf4505823
Stephen <wizebin@gmail.com>
2018-05-16 18:46:03 -0400

    Using new wrapAction function

--- src/index.js
+++ src/index.js
@@ -1,26 +1,17 @@
-import nexusdk from 'nexusdk';
+import { wrapAction } from 'nexusdk';
 import notifier from 'node-notifier';
-
-function callWrap(messageType) {
-  return function (message) {
-    nexusdk.sendMessage({ type: messageType, data: message });
-  }
-}
 
 const boundNotify = notifier.notify.bind(notifier);
 
-nexusdk.on('start', (properties) => {
+wrapAction((properties) => {
   const { title, message, timeout } = properties;
+
   const result = boundNotify({
     title,
     message: message ? message : ' ',
     timeout: timeout ? timeout : undefined,
-  }, () => {
-    process.exit(1);
   });
-});
 
-nexusdk.on('exit', () => {
-  process.exit(1);
+  return !!result;
 });
 
